2ab4c9316490cfa303b76d5fafec2595064ef992
blyxyas <blyxyas@gmail.com>
2023-05-12 14:45:54 +0200

    test: Now the `fmt` test uses `empty_stdout` instead of `with_stdout("")`

--- tests/fmt.rs
+++ tests/fmt.rs
@@ -1,12 +1,18 @@
+use better_panic::Settings;
 use cli_sandbox::WithStdout;
 use std::process::Command;
 
 #[test]
 fn fmt() {
+    Settings::new()
+        .verbosity(better_panic::Verbosity::Minimal)
+        .install();
     let cmd = Command::new("cargo")
         .args(["fmt", "--check"])
         .output()
         .expect("Couldn't check formatting (`cargo fmt --check`)");
 
-    cmd.with_stdout("");
+    if !cmd.empty_stdout() {
+        panic!("`cargo fmt --check` stdout wasn't empty, run `cargo fmt` to format the project");
+    };
 }
