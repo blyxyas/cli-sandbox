349c8f87523bfe85025c953e0f92d710a32b492b
blyxyas <blyxyas@gmail.com>
2023-05-12 17:16:16 +0200

    test: Add `project.rs` test.

---
+++ tests/project.rs
@@ -0,0 +1,18 @@
+use cli_sandbox::{project};
+use better_panic;
+use pretty_assertions::assert_ne;
+
+#[test]
+fn new_project() {
+	better_panic::install();
+	let _ = project().expect("Couldn't create a new project");
+}
+
+#[test]
+fn multiple_projects_different_paths() {
+	better_panic::install();
+	let proj1 = project().expect("Couldn't create a new project");
+	let proj2 = project().expect("Couldn't create a new project");
+
+	assert_ne!(proj1.path(), proj2.path());
+}
